56b9c72b83d4cc9565081adef63a650464a5f469
akashjaiswal36 <akash.jaiswal36@gmail.com>
2026-02-11 15:21:42 +0530

    Revert "Simulate failure in Lambda for rollback drill"
    This reverts commit dc85d9b99c2144e30877bb1e0ada8ae4632d43f5.

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,2 +1,22 @@
+import json
+import boto3
+import uuid
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("sre-messages")
+
 def handler(event, context):
-    raise Exception("Simulated failure")
+    body = json.loads(event.get("body", "{}"))
+    message = body.get("message", "hello")
+
+    table.put_item(
+        Item={
+            "id":str(uuid.uuid4()),
+            "message": message
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"status": "ok", "message": message})
+    }
